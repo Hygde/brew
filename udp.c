5959d543e333b6f00d7366819472d5d221c00b80
Hygde <samuel.devals@outlook.fr>
2022-05-24 11:14:50 +0200

    put code into functions

--- udp.c
+++ udp.c
@@ -1,52 +1,81 @@
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int argc, char*argv[]) {
-    int rc = 0;
-    int sock = 0;
+#define RX_BUFFER_SIZE 256
+
+int createSocket() {
+    int ret = socket(AF_INET6, SOCK_DGRAM, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Fail to create socket! socket return %d", ret);
+        exit(ret);
+    }
+    return ret;
+}
+
+void bindSocketToPort(int sock, uint16_t port) {
     struct sockaddr_in6 saddr;
-    struct sockaddr_in6 caddr;
-    socklen_t caddrlen = sizeof(caddr);
+    int ret;
 
     memset((char *)&saddr, 0, sizeof(saddr));
     saddr.sin6_family = AF_INET6;
     saddr.sin6_addr = in6addr_any;
-    saddr.sin6_port = htons(2000);
+    saddr.sin6_port = htons(port);
 
-    sock = socket(AF_INET6, SOCK_DGRAM, 0);
-    if(sock >= 0) {
-        if(bind(sock, (struct sockaddr *) &saddr, sizeof(saddr)) >= 0){
-            uint8_t buf[2] = {0};
-            if(recvfrom(sock, buf, sizeof(uint16_t), 0, (struct sockaddr *) &caddr, &caddrlen) > 0) {
-                uint16_t*port = (uint16_t*) buf;
-                printf("Received from: %x:%x:%x:%x:%x:%x:%x:%x\n",
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[0]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[1]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[2]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[3]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[4]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[5]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[6]),
-                    ntohs(caddr.sin6_addr.__in6_u.__u6_addr16[7])
-                );
-                printf("Received port: %u\n", ntohs(*port));
-            } else {
-                perror("can not recvfrom\n");
-                rc = EIO;
-            }
-        } else {
-            perror("Can not bind");
-            rc = EIO;
-        }
-    } else {
-        perror("Can not create socket\n");
-        rc = EIO;
+    ret = bind(sock, (struct sockaddr*)&saddr, sizeof(saddr));
+    if (ret < 0) {
+        fprintf(stderr, "Fail to bind socket! bind returned %d", ret);
+        exit(ret);
     }
+}
+
+ssize_t receiveFrom(int sock, uint8_t*buf, struct sockaddr_in6*src_addr) {
+    ssize_t ret;
+    socklen_t src_addrlen = sizeof(struct sockaddr_in6);
+
+    if(buf == NULL) {
+        fprintf(stderr, "Buffer is a null pointer %hhn", buf);
+        exit(EIO);
+    }
+
+    ret = recvfrom(sock, buf, RX_BUFFER_SIZE, 0, (struct sockaddr *) src_addr, &src_addrlen);
+    if (ret <= 0) {
+        fprintf(stderr, "Fail to receive data! recvfrom returned %ld", ret);
+        exit(ret);
+    } else {
+        printf("Received from: %x:%x:%x:%x:%x:%x:%x:%x\n",
+            ntohs(src_addr->sin6_addr.s6_addr16[0]),
+            ntohs(src_addr->sin6_addr.s6_addr16[1]),
+            ntohs(src_addr->sin6_addr.s6_addr16[2]),
+            ntohs(src_addr->sin6_addr.s6_addr16[3]),
+            ntohs(src_addr->sin6_addr.s6_addr16[4]),
+            ntohs(src_addr->sin6_addr.s6_addr16[5]),
+            ntohs(src_addr->sin6_addr.s6_addr16[6]),
+            ntohs(src_addr->sin6_addr.s6_addr16[7])
+        );
+    }
+
+    return ret;
+}
+
+int main(int argc, char*argv[]) {
+    int rc = 0;
+    int sock = createSocket();
+    uint8_t buf[RX_BUFFER_SIZE];
+    uint8_t datalen;
+    struct sockaddr_in6 caddr;
+    socklen_t caddrlen = sizeof(caddr);
+
+    bindSocketToPort(sock, 2000);
+
+    datalen = receiveFrom(sock, buf,&caddr);
+    printf("Received port: %u\n", ntohs(*(uint16_t*)buf));
 
     close(sock);
 
