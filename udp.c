e25d110dcfdaf4c4ca8e1640cf77139527e9ff38
Hygde <samuel.devals@outlook.fr>
2022-05-25 11:28:45 +0200

    update includes of udp.c

--- udp.c
+++ udp.c
@@ -1,10 +1,9 @@
-#include <errno.h>
-#include <stdint.h>
-#include <stdio.h>
+#include "udp.h"
 #include <stdlib.h>
-#include <sys/types.h>
+#include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
