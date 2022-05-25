c2954e4c3f3d7b8a720e162e630f55ecbe96adaa
Hygde <samuel.devals@outlook.fr>
2022-05-25 11:27:17 +0200

    Add udp header file

---
+++ udp.h
@@ -0,0 +1,44 @@
+#ifndef __UDP_H__
+#define __UDP_H__
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct sockaddr_in6;
+
+/**
+ * @brief Create a Socket object
+ * 
+ * @return int 
+ */
+int createSocket();
+
+/**
+ * @brief Bind a given socket to a specific port
+ * 
+ * @param sock 
+ * @param port 
+ */
+void bindSocketToPort(int sock, uint16_t port);
+
+/**
+ * @brief Receive data using a given socket
+ * 
+ * @param sock 
+ * @param buf 
+ * @param src_addr 
+ * @return ssize_t 
+ */
+ssize_t receiveFrom(int sock, uint8_t*buf, struct sockaddr_in6*src_addr);
+
+/**
+ * @brief Send data using a given socket
+ * 
+ * @param sock 
+ * @param buf 
+ * @param len 
+ * @param dst_addr 
+ */
+void sendTo(int sock, uint8_t*buf, uint8_t len, struct sockaddr_in6*dst_addr);
+
+#endif/*__UDP_H__*/
