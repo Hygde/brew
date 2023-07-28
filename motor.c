48fbe39e56d6f60a6c7f0d52d8d5b9f567aedfbd
Hygde <samuel.devals@outlook.fr>
2023-07-28 19:28:16 +0200

    bypass rpi gpio code for x86 (testing purpose)

--- motor.c
+++ motor.c
@@ -2,30 +2,40 @@
 #include "motor.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <bcm2835.h>
 #include <errno.h>
 
+#ifndef __x86_64__
+#include <bcm2835.h>
+#endif
+
 void initBcm2835() {
+#ifndef __x86_64__
     int ret = bcm2835_init();
     if (!ret) {
         fprintf(stderr, "Fail to init bcm2835 library\nError code : %d\n", errno);
         exit(errno);
     }
+#endif
 }
 
 void initMotor(const int pin) {
+#ifndef __x86_64__
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
+#endif
 }
 
 void motorChangeMode(const int pin) {
+#ifndef __x86_64__
     bcm2835_gpio_write(pin, 1);
     bcm2835_delay(500);
     bcm2835_gpio_write(pin, 0);
-
+#endif
 }
 
 void closeMotor() {
+#ifndef __x86_64__
     bcm2835_close();
+#endif
 }
 
 #ifdef UNIT_TEST
